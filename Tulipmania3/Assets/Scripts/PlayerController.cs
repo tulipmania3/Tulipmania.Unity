d6f49a51abac84eee1b6b296ab3228fbd968a220
Sam Smeal <ssmeal@fsiservices.com>
2019-07-09 18:32:10 -0400

    Animations

--- Tulipmania3/Assets/Scripts/PlayerController.cs
+++ Tulipmania3/Assets/Scripts/PlayerController.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-   Rigidbody2D rigidbody2d;
     
+    public float speed;
+    Rigidbody2D rigidbody2d;
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -17,10 +21,13 @@ public class PlayerController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        
+
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 4.5f* horizontal * Time.deltaTime;
-        position.y = position.y + 4.5f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        animator.SetFloat("Move X", horizontal);
+        animator.SetFloat("Move Y", vertical);
 
         rigidbody2d.MovePosition(position);
     }
