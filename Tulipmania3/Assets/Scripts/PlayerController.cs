1808e3226d9d66a853dd8579d50595d310f87b08
Sam Smeal <ssmeal@fsiservices.com>
2019-07-09 12:34:37 -0400

    Basic Unity Project

---
+++ Tulipmania3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   Rigidbody2D rigidbody2d;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 4.5f* horizontal * Time.deltaTime;
+        position.y = position.y + 4.5f * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+}
