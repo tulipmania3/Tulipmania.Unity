eaf75f93a279fb7029413279da7069cbe5fbd816
Sam Smeal <ssmeal@fsiservices.com>
2019-07-11 11:12:59 -0400

    Greenhouse changes and build run

---
+++ Tulipmania3/Assets/Scripts/GreenhouseController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Greenhouse {
+    public string name;
+    public float value;
+    
+}
+
+public class GreenhouseController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Tulipmania3/Assets/Scripts/Tulip.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tulip
+{
+    public string name;
+    public Time TimeCreated;
+    public Time TimeBought;
+}
